121869185d2c720e5c22a011f6579538f0675afd
Cristina <Cristina@DESKTOP-G273TOE>
2023-06-15 13:55:02 +0200

    xxxxxxxxxxxxx

---
+++ proyectoNetBeans/src/main/java/com/mycompany/proyectonetbeans/ProyectoNetBeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectonetbeans;
+
+/**
+ *
+ * @author Cristina
+ */
+public class ProyectoNetBeans {
+
+    	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Estamos realizando un control de versiones con la herramienta Git");
+		
+		System.out.println("Este mensaje es para continuar con la practica y modificar el programa");
+        }
+		
+}
