f98fbc02e7c78ed3294c25d2951750da98734aa2
Cristina <Cristina@DESKTOP-G273TOE>
2023-06-15 14:23:16 +0200

    xxxxxxxxxxxxx

--- proyectoNetBeans/src/main/java/com/mycompany/proyectonetbeans/ProyectoNetBeans.java
+++ proyectoNetBeans/src/main/java/com/mycompany/proyectonetbeans/ProyectoNetBeans.java
@@ -7,14 +7,53 @@ package com.mycompany.proyectonetbeans;
 /**
  *
  * @author Cristina
+ * @version v1.2
  */
 public class ProyectoNetBeans {
+    /**
+     * 
+     */
+public class Counter {
+/**
+ * 
+ */
+    private int count;
+// a simple integer instance variable
+/**
+ * 
+ */
+    public Counter() { }
+// default constructor (count is 0)
+/**
+ * 
+ * @param initial valor inicial del contador
+ */
+    public Counter(int initial) { count = initial; }
+// an alternate constructor
+/**
+ * 
+ * @return 
+ */
+    public int getCount() { return count; }
+// an accessor method
+/**
+ * 
+ */
+    public void increment() { count++; }
+// an update method
+/**
+ * 
+ * @param delta 
+ */
+    public void increment(int delta) { count += delta; }
+// an update method
+/**
+ * 
+ */
+    public void reset() { count = 0; }
+// an update method
 
-    	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Estamos realizando un control de versiones con la herramienta Git");
-		
-		System.out.println("Este mensaje es para continuar con la practica y modificar el programa");
-        }
+}
+
 		
 }
